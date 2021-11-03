b4d8ae63ec456d01c17ed67743f304dfec74a6f0
Eka-G <ekaglazkovaa@yandex.ru>
2021-11-03 11:41:43 +0300

    feat: add carousel

---
+++ components/Carousel.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-carousel class="carousel" cycle>
+    <v-carousel-item
+      v-for="(image, index) in images"
+      :key="index"
+      :src="image"
+    ></v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      images: ['/slide-1.svg', '/slide-2.svg', '/slide-3.svg'],
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+.carousel {
+  max-width: 900px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <template>
   <v-container col="12" class="pa-0">
-    <HeroImage />
+    <HeroImage class="mb-12" />
+
+    <section class="mb-12 d-flex flex-column justify-center align-center">
+      <h2>Demonstration</h2>
+      <p>Check out our wonderful examples:</p>
+
+      <Carousel col="6" />
+    </section>
   </v-container>
 </template>
